f5034820c6231259ea96f4e8bba1f2bfe5a57a61
Ausmarton Fernandes <ausmarton@gmail.com>
2011-07-15 18:27:29 +0530

    Added some styles to the theme austeriofernandes. In order to view these changes, you might need to create certain blocks and nodes. Or else everything will appear as a small box.

--- drupal-7.4/sites/default/themes/austeriofernandes/page.tpl.php
+++ drupal-7.4/sites/default/themes/austeriofernandes/page.tpl.php
@@ -1,17 +1,20 @@
 <div id="container">
-	<div class="header">
+	<div id="header">
 		<div class="logo">
 			<a href="/">austeriofernandes</a>
 		</div>
 		<?php print render($page['header']);?>
 	</div>
-	<div class="column content">
-		<?php print render($page['content']);?>
-	</div>
-	<div class="column sidebar">
-		<?php print render($page['sidebar']); ?>
+	<div id="content-area">
+		<div id="main-content" class="column">
+			<?php print render($page['content']);?>
+		</div>
+		<div id="sidebar" class="column">
+			<?php print render($page['sidebar']); ?>
+		</div>
 	</div>
 	<div id="footer">
 		<?php print render($page['footer']); ?>
+		<div id="copyright"> © 2011 Austerio Fernandes</div>
 	</div>
 </div>
