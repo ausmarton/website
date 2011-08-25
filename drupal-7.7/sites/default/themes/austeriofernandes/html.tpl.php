30a0d73814c8b7b37b700b8e0eabeabe334ff0f2
Ausmarton Fernandes <ausmarton@gmail.com>
2011-08-25 18:09:11 +0530

    Changed from db_query to drupal_write_record for inserting new visitor info. Form needs to be styled.

--- drupal-7.7/sites/default/themes/austeriofernandes/html.tpl.php
+++ drupal-7.7/sites/default/themes/austeriofernandes/html.tpl.php
@@ -3,6 +3,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 
 <head profile="<?php print $grddl_profile; ?>">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <META name="description" CONTENT = "Austerio Fernandes is an Investment Consultant cum Advisor having more than 27 years of experience with Life Insurance Corporation of India (LIC). He caters to over 1000 clients all over the State of Goa, some in other parts of India, and many more in Gulf , UK, Portugal, and other countries. He can provide you accurate tailor made package plans for managing your investments successfully. He can provide any or all consultations and advice on the vexed portfolio of Financial Planning and Investment in diversified Instruments, viz. Life Insurance, Mutual funds and Share Market trading." />
+  <META name="keywords" CONTENT = "Life Insurance Corporation,LIC,insurance,investment,goa,share market,consultant,mutual funds,austerio,fernandes,27 years,1000 clients,finance,planning" />
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
@@ -14,5 +17,20 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
+  
+  <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-7860845-3']);
+  _gaq.push(['_setDomainName', '.austeriofernandes.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+  </script>
 </body>
 </html>
--- drupal-7.7/sites/default/themes/austeriofernandes/js/script.js
+++ drupal-7.7/sites/default/themes/austeriofernandes/js/script.js
@@ -1,8 +1,9 @@
+var $ = jQuery.noConflict();
+
 var mobile='+91 9422449858';
 var email='austerio_fernandes@yahoo.co.uk';
 			
 $(document).ready(function() {
 	$(".mobile .mobile").text(mobile.toString());
-	$(".email .email").text(email.toString());
+	$(".email .email").html('<a href="mailto:'+email.toString()+'">'+email.toString()+'</a>');
 });
-
